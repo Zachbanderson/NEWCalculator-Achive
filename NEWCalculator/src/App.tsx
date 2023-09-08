74e180597d351827b8164c2d5650a0fda552e4ef
Zachbanderson <zachbanderson@gmail.com>
2023-09-07 19:42:08 -0700

    Feat: Add dropdown with options for explosives

--- NEWCalculator/src/App.tsx
+++ NEWCalculator/src/App.tsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import explosives from './explosives.json'
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
+    type explosive = {
+        name: string;
+        ref: number;
+    };
 
-  return (
-    <>
-      <h1>NEW Calculator</h1>
-    </>
-  )
+    function loadJsonExplosives(explosivesData)
+    {
+        return explosivesData.map((e) => {
+            return {name: e.Name, ref: e.REF}
+        })
+    }
+    let explosivesList: Array<explosive> = loadJsonExplosives(explosives.Explosives)
+
+    const [currentExplosive, setCurrentExplosive] = useState({name:'', ref: 0})
+    return (
+        <>
+            <h1>NEW Calculator</h1>
+
+            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                {explosivesList.map(explosive => {
+                    return <Dropdown.Item key={crypto.randomUUID()} onClick={() => setCurrentExplosive(explosive)}>{explosive.name}</Dropdown.Item>
+                    }
+                )}
+            </DropdownButton>
+            <p>Current explosive is {currentExplosive.name}</p>
+        </>
+    )
 }
 
 export default App
